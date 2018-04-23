aecb7550cbe66fbbfe411675c0de16021e220ee4
Chad Burt <chad@underbluewaters.net>
2018-04-23 13:31:52 -1000

    Added results queue handling

--- src/handler.js
+++ src/handler.js
@@ -1,17 +1,52 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({ region: process.env.AWS_REGION });
+var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+
+var sqsParams = {
+  DelaySeconds: 0,
+  QueueUrl: process.env.RESULTS_SQS_ENDPOINT
+};
+
 // Wraps geoprocessing functions to meet lambda requirements
 module.exports = geoprocessor => {
+  const startTime = new Date().getTime();
   return async (event, context) => {
-    //
-    var fs = event.body ? JSON.parse(event.body) : event;
+    var fs;
+    if (event.body) {
+      if (typeof event.body === 'string') {
+        fs = JSON.parse(event.body);
+      } else {
+        fs = event.body;
+      }
+    } else {
+      fs = event;
+    }
+    if (event.invocationId) {
+      console.log(`invocationId: ${event.invocationId}`)
+    }
     const response = await geoprocessor(fs);
-    return {
-      statusCode: 200,
-      isBase64Encoded: false,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true
-      },
-      body: JSON.stringify(response)
+    const results = {
+      results: response,
+      duration: new Date().getTime() - startTime,
+      requestId: context.awsRequestId,
+      invocationId: event.invocationId
     };
+    if (event.postToSQS) {
+      return sqs.sendMessage({
+        ...sqsParams,
+        MessageBody: JSON.stringify(results)
+      }).promise();
+    } else {
+      return {
+        statusCode: 200,
+        isBase64Encoded: false,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
+        },
+        body: JSON.stringify(results)
+      };
+    }
   };
 };
