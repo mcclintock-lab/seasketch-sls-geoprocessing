30f974df39ecaa1df3dec7632caed2c1977e027e
Chad Burt <chad@underbluewaters.net>
2018-06-06 11:51:33 -1000

    Starting to send info to sqs

--- bin/run-ami-tasks.js
+++ bin/run-ami-tasks.js
@@ -1,21 +1,74 @@
 #!/usr/bin/env node
-const { spawn } = require('child_process');
-const worker = spawn('stdbuf', ['-i0','-o0','-e0', //disable all buffering
-  'bash', process.argv[2]]);
+const { spawn } = require("child_process");
+const AWS = require("aws-sdk");
+const { FAILURE } = require("../src/constants");
+// Set the region
+AWS.config.update({ region: process.env.AWS_DEFAULT_REGION });
+const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
 
-worker.stdout.on('data', (data) => {
-  console.log("\x1b[45m", data.toString());
+var messages = [];
+
+const sendMessages = async () => {
+  const incoming = messages;
+  messages = [];
+  if (incoming.length) {
+    console.log(`sending ${incoming.length} messages`);
+    const body = {
+      amiHandler: process.env.INVOCATION_ID,
+      messages: incoming
+    };
+    try {
+      await sqs
+        .sendMessage({
+          MessageBody: JSON.stringify(body),
+          QueueUrl: process.env.LOGS_SQS_ENDPOINT,
+          DelaySeconds: 0
+        })
+        .promise();
+    } catch (e) {
+      console.error("Failed to send messages to sqs");
+      messages = [...incoming, ...messages];
+    }
+  } else {
+    return;
+  }
+};
+
+const message = (msg, type) => {
+  return {
+    timestamp: new Date().getTime(),
+    message: msg,
+    type: type || "stdout"
+  };
+};
+
+setInterval(sendMessages, 200);
+
+const worker = spawn("stdbuf", [
+  "-i0",
+  "-o0",
+  "-e0", //disable all buffering
+  "bash",
+  process.argv[2]
+]);
+
+worker.stdout.on("data", data => {
+  messages.push(message(data.toString(), "stdout"));
 });
 
-worker.stderr.on('data', (data,e) => {
-  if (data.toString().indexOf('+') === 0) {
-    console.log('\x1b[36m%s\x1b[0m', data.toString().replace(/(^|\n)[\+]*\s/g, '$1'));
+worker.stderr.on("data", (data, e) => {
+  if (data.toString().indexOf("+") === 0) {
+    messages.push(
+      message(data.toString().replace(/(^|\n)[\+]*\s/g, "$1"), "command")
+    );
   } else {
-    console.log('\x1b[33m%s\x1b[0m', data.toString());
+    messages.push(message(data.toString(), "stderr"));
   }
 });
 
-worker.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-  process.exit(0);
+worker.on("close", code => {
+  if (code !== 0) {
+    messages.push(message(`${FAILURE}: child process exited with code ${code}`));
+  }
+  sendMessages().then(() => process.exit(0));
 });
